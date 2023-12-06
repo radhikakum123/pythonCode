0a297e76dd500d6dc078ea6d28e17f8aca569814
radhikakum123 <radhika.kumbhojkar@gmail.com>
2023-12-06 17:22:18 +0530

    Add files via upload

---
+++ 1stone.py
@@ -0,0 +1,14 @@
+a=100
+
+print(a,type(a),id(a))
+#print(id(z))
+#to check type of identifier use type keyword.
+#each object has memory  to check memory address use id keyword.
+
+
+b=77.80
+print(type(b))
+
+print(10,20,30,sep=' ')
+print(10,20,30,sep='\t')
+print(10,20,30,sep='\n')

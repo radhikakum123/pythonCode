18b217b4cf8de99fbf6e68de414a57b69fef48c1
radhikakum123 <radhika.kumbhojkar@gmail.com>
2023-12-07 11:07:41 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,22 @@
+a=100
+print(a+100)
+print(a-20)
+#Membership Operators
+# 'dil' in 'dil to pagal hai'
+True
+#>>> 'xyz' in 'dil to pagal hai'
+False
+#>>> 1 not in [1,2,3,4,5]
+False
+#>>> 100 not in [1,2,3,4,5]
+True
+#Identity Operators
+#>>> "laptop" is "laptop"
+True
+#>>> "laptop" is "mouse"
+False
+#>>> "laptop" is not "mouse"
+True
+#>>> "laptop" is not "laptop"
+False
+
